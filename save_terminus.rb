27c871e24848d17000a3ceffb54b25758f48c178
Jason Liu <jasonliu20194@gmail.com>
2019-09-04 14:32:48 +1000

    second commit

--- save_terminus.rb
+++ save_terminus.rb
@@ -1,57 +1,15 @@
 # Main menu
 require 'tty-prompt'
 require 'tty-font'
+require_relative 'stage1'
+
 prompt = TTY::Prompt.new
 font = TTY::Font.new(:doom)
 puts font.write("SAVE TERMINUS")
 health = 100
 module Game
     #Stage 1
-    def self.stage1(health)
-        stage = 1
-
-       while health != 0
-        play_option = gets.chomp.to_i
-        puts `clear`
-        puts "Health = #{health}"
-        puts "Stage = #{stage}"
-        puts "You are in the the forest of the death. You quickly look around and behind you appears a very scary ghost. Make a Decision!:"
-        #options
-        puts "1) Run away"
-        puts "2) Panic"
-        puts "3) Stab the Ghost"
-        puts "4) Light up a fire"
-        play_option = gets.chomp.to_i
-        if  play_option == 1
-            puts `clear`
-            puts "Health = #{health -= 25}"
-            puts "Stage = #{stage}"
-            puts "You tried to run but the ghost catches up to you and you get so scared you run into a tree. You have lost some of your health"
-            return
-        elsif
-            play_option == 2
-            puts `clear`
-            puts "Health = #{health -= 25}"
-            puts "Stage = #{stage}"
-            puts "You panic and trip over a rock. You have lost some of your health"
-            return
-        elsif
-            play_option == 3
-            puts `clear`
-            puts "Health = #{health -= 50}"
-            puts "Stage = #{stage}"
-            puts "You stab the ghost however it does nothing to the ghost. You swing so hard that you accidently stab yourself. You have lost half your health!"
-            return
-        elsif
-            play_option == 4
-            puts `clear`
-            puts "Health = #{health}"
-            puts "Stage = #{stage}"
-            puts "You light up a fire and the ghost gets scared away. You have made it safely out of the forest of death."
-            return
-        end
-    end
-    end
+    
     def self.stage2(health)
         stage = 2
     
@@ -72,28 +30,28 @@ module Game
             puts "Health = #{health -= 25}"
             puts "Stage = #{stage}"
             puts "You throw ice at the polar bear. He gets angry and chases you out of the cave. Whilst running for your life you slip over on ice and almost break your legs. You have lost some of your health"
-            return
+            return health
         elsif
             play_option == 2
             puts `clear`
             puts "Health = 0"
             puts "Stage = #{stage}"
             puts "You give food to the polar bear but after eating the food it is still hungry. It eats you for dinner."
-            return
+            return health
         elsif
             play_option == 3
             puts `clear`
             puts "Health = #{health -= 50}"
             puts "Stage = #{stage}"
             puts "You growl pretending to be a polar bear. The mutated polar bear growls too and within a few seconds appears its whole family. They all trample over you. You have lost a substancial amount of health!"
-            return
+            return health
         elsif
             play_option == 4
             puts `clear`
             puts "Health = #{health}"
             puts "Stage = #{stage}"
             puts "You pretend to have been fatally injured and collapse. The polar bear walks away and once you see that he is completely out of your sight, you hurry out of the cave."
-            return
+            return health
         end
     end
 end
@@ -107,8 +65,9 @@ def main_menu(prompt,health)
             user_name = gets.chomp
             puts `clear`
             puts "Hello #{user_name}. Welcome to the world of Terminus, where you must save the world from ending. You will fight many monsters during your journey here. Survive through all the monsters and you will be able to reach the Evil King, who is solely responsible for all of this. Defeat the King in order to save your world. Throughout the game, you will encounter many fights along the way. Decision making is crucial in this game, as choosing the wrong decisions can lead to uneventful results, and may even cause death. You will start off with 100 health. Once your hp reaches 0 it is game over. So plan every decision very carefully. Please press enter to continue"
-            Game.stage1(health)
-            Game.stage2(health)
+            health = StageOne.run(health)
+            health = Game.stage2(health)
+
         elsif main_menu_option == About
             puts "You are in the world named Terminus. Terminus has been invaded by many monsters. Your goal is to travel the world whilst fight off all the monsters all over Terminus and defeat the evil King and save the world from doom. 
         "
--- stage1.rb
+++ stage1.rb
@@ -1,43 +1,91 @@
-# stage 1
-def stage1()
-    health = 100
-    stage = 1
-    play_option = gets.chomp.to_i
-    puts `clear`
-    puts "Health = #{health}"
-    puts "Stage = #{stage}"
-    puts "You are in the the forest of the death. You quickly look around and behind you appears a very scary ghost. Make a Decision!:"
-    #options
-    puts "1) Run away"
-    puts "2) Panic"
-    puts "3) Stab the Ghost"
-    puts "4) Light up a fire"
-    play_option = gets.chomp.to_i
-    if play_option == 1
-        puts `clear`
-        puts "Health = #{health - 25}"
-        puts "Stage = #{stage}"
-        puts "You tried to run but the ghost catches up to you and you get so scared you run into a tree. You have lost some of your health"
-    elsif
-        play_option == 2
-        puts `clear`
-        puts "Health = #{health - 25}"
-        puts "Stage = #{stage}"
-        puts "You panic and trip over a rock. You have lost some of your health"
-    elsif
-        play_option == 3
-        puts `clear`
-        puts "Health = #{health - 50}"
-        puts "Stage = #{stage}"
-        puts "You stab the ghost however it does nothing to the ghost. You swing so hard that you accidently stab yourself. You have lost half your health!"
-    elsif
-        play_option == 4
+# # stage 1
+# def stage1()
+#     health = 100
+#     stage = 1
+#     play_option = gets.chomp.to_i
+#     puts `clear`
+#     puts "Health = #{health}"
+#     puts "Stage = #{stage}"
+#     puts "You are in the the forest of the death. You quickly look around and behind you appears a very scary ghost. Make a Decision!:"
+#     #options
+#     puts "1) Run away"
+#     puts "2) Panic"
+#     puts "3) Stab the Ghost"
+#     puts "4) Light up a fire"
+#     play_option = gets.chomp.to_i
+#     if play_option == 1
+#         puts `clear`
+#         puts "Health = #{health - 25}"
+#         puts "Stage = #{stage}"
+#         puts "You tried to run but the ghost catches up to you and you get so scared you run into a tree. You have lost some of your health"
+#     elsif
+#         play_option == 2
+#         puts `clear`
+#         puts "Health = #{health - 25}"
+#         puts "Stage = #{stage}"
+#         puts "You panic and trip over a rock. You have lost some of your health"
+#     elsif
+#         play_option == 3
+#         puts `clear`
+#         puts "Health = #{health - 50}"
+#         puts "Stage = #{stage}"
+#         puts "You stab the ghost however it does nothing to the ghost. You swing so hard that you accidently stab yourself. You have lost half your health!"
+#     elsif
+#         play_option == 4
+#         puts `clear`
+#         puts "Health = #{health}"
+#         puts "Stage = #{stage}"
+#         puts "You light up a fire and the ghost gets scared away. You have made it safely out of the forest of death."
+#     else
+#         puts "Please select a valid option"
+#         play_option = gets.chomp.to_i 
+#     end
+# end
+
+module StageOne
+    def self.run(health)
+        stage = 1
+
+    while health != 0
+        play_option = gets.chomp.to_i
         puts `clear`
         puts "Health = #{health}"
         puts "Stage = #{stage}"
-        puts "You light up a fire and the ghost gets scared away. You have made it safely out of the forest of death."
-    else
-        puts "Please select a valid option"
-        play_option = gets.chomp.to_i 
+        puts "You are in the the forest of the death. You quickly look around and behind you appears a very scary ghost. Make a Decision!:"
+        #options
+        puts "1) Run away"
+        puts "2) Panic"
+        puts "3) Stab the Ghost"
+        puts "4) Light up a fire"
+        play_option = gets.chomp.to_i
+        if  play_option == 1
+            puts `clear`
+            puts "Health = #{health -= 25}"
+            puts "Stage = #{stage}"
+            puts "You tried to run but the ghost catches up to you and you get so scared you run into a tree. You have lost some of your health"
+            return health
+        elsif
+            play_option == 2
+            puts `clear`
+            puts "Health = #{health -= 25}"
+            puts "Stage = #{stage}"
+            puts "You panic and trip over a rock. You have lost some of your health"
+            return health
+        elsif
+            play_option == 3
+            puts `clear`
+            puts "Health = #{health -= 50}"
+            puts "Stage = #{stage}"
+            puts "You stab the ghost however it does nothing to the ghost. You swing so hard that you accidently stab yourself. You have lost half your health!"
+            return health
+        elsif
+            play_option == 4
+            puts `clear`
+            puts "Health = #{health}"
+            puts "Stage = #{stage}"
+            puts "You light up a fire and the ghost gets scared away. You have made it safely out of the forest of death."
+            return health
+        end
     end
-end
+    end
+end 
